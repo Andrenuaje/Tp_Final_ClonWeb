4afdeafb3693b3a170e14b08694e5f5bde10ea57
Andres <simonolivia12@gmail.com>
2025-09-09 19:17:57 -0300

    Fotos

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import { Footer } from './Components/Footer/Footer'
 import { OurTeam } from './Components/OurTeam/OurTeam'
 import CurvedArrow from './Components/Linea'
 
+import logo from '../assets/img/logo.png';
+
+
 
 
 
@@ -36,7 +39,7 @@ function App() {
         <nav className="flex justify-between items-center p-6 max-w-7xl mx-auto font-bogart">
           <div className="flex items-center">
             <img 
-              src="/img/logo.png" 
+              src={logo}
               alt="Ta: Logo" 
               className="h-20 w-auto"
             />
