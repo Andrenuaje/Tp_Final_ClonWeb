177db1f73c93411235b3e55667949e5ab2780006
Andres <simonolivia12@gmail.com>
2025-09-10 16:49:47 -0300

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
 import { Estadistica } from './Components/Estadisticas/Estadistica'
 import { Noticias } from './Components/Noticias/Noticias'
 
